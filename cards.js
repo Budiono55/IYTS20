b9a65ecf348bdc1dfbab859e8d510909e129c547
Budiono55 <48086229+Budiono55@users.noreply.github.com>
2022-04-25 14:59:45 +0700

    Add files via upload

--- cards.js
+++ cards.js
@@ -2,37 +2,30 @@ const cards = [
     {
         "name": "day1",
         "title": "Day 1",
-        "img": "./img/Timeline/day1.jpg",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, recusandae?"
     }, {
         "name": "day2",
         "title": "Day 2",
-        "img": "./img/Timeline/day2.jpg",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, recusandae?"
     }, {
         "name": "day3",
         "title": "Day 3",
-        "img": "./img/Timeline/day3.jpg",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, recusandae?"
     }, {
         "name": "day4",
         "title": "Day 4",
-        "img": "./img/Timeline/day4.jpg",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, recusandae?"
     }, {
         "name": "day5",
         "title": "Day 5",
-        "img": "./img/Timeline/day5.jpg",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, recusandae?"
     }, {
         "name": "day6",
         "title": "Day 6",
-        "img": "./img/Timeline/day6.jpg",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, recusandae?"
     }, {
         "name": "day7",
         "title": "Day 7",
-        "img": "./img/Timeline/day2.jpg",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, recusandae?"
     }
 ]
